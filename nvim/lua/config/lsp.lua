746c786f4d5381b6aa4133c811d9782c2414f69d
Anton Müller <antxn.07@gmail.com>
2026-04-03 07:23:54 -0400

    enhance lsp keybindings

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,49 +1,72 @@
+local format = {
+    c = "clangd",
+    cpp = "clangd",
+    css = "biome",
+    javascript = "biome",
+    javascriptreact = "biome",
+    json = "biome",
+    jsonc = "biome",
+    lua = "lua_ls",
+    rust = "rust_analyzer",
+    typescriptreact = "biome",
+    typescript = "biome",
+    python = "pyright",
+    zig = "zls",
+}
+
+local logic = {
+    c = "clangd",
+    cpp = "clangd",
+    css = "cssls",
+    javascript = "vtsls",
+    javascriptreact = "vtsls",
+    json = "jsonls",
+    jsonc = "jsonls",
+    lua = "lua_ls",
+    rust = "rust_analyzer",
+    typescriptreact = "vtsls",
+    typescript = "vtsls",
+    python = "pyright",
+    zig = "zls",
+}
+
 vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
         local bufnr = args.buf
         local client = vim.lsp.get_client_by_id(args.data.client_id)
-
+        local ft = vim.bo[args.buf].filetype;
         if not client then return end
 
-        local caps = client.server_capabilities
+        local format_client = format[ft];
+        local logic_client = logic[ft]
 
-        if not caps then return end
+        ---@param severity vim.diagnostic.Severity | nil
+        local function bind_qf_diagnostics(keys, severity, desc)
+            vim.keymap.set(
+                "n",
+                keys,
+                function()
+                    vim.diagnostic.setqflist({ open = true, severity = severity })
+                end,
+                { buffer = bufnr, noremap = true, silent = true, desc = desc }
+            )
+        end
 
-        vim.keymap.set(
-            "n",
-            "gd",
-            vim.lsp.buf.definition,
-            { buffer = bufnr, noremap = true, silent = true, desc = "LSP: Go to definition" }
-        )
+        bind_qf_diagnostics('<leader>la', nil, 'LSP: Show qf diagnostics');
+        bind_qf_diagnostics('<leader>le', "ERROR", 'LSP: Show qf errors');
+        bind_qf_diagnostics('<leader>lw', "WARN", 'LSP: Show qf warning');
+        bind_qf_diagnostics('<leader>lh', "HINT", 'LSP: Show qf hints');
 
-        vim.keymap.set(
-            "n",
-            "<leader>la",
-            function()
-                vim.diagnostic.setqflist({ open = true })
-            end,
-            { buffer = bufnr, noremap = true, silent = true, desc = "LSP: Show all diagnostics" }
-        )
+        if client.name == logic_client then
+            vim.keymap.set(
+                "n",
+                "gd",
+                vim.lsp.buf.definition,
+                { buffer = bufnr, noremap = true, silent = true, desc = "LSP: Go to definition" }
+            )
+        end
 
-        vim.keymap.set(
-            "n",
-            "<leader>le",
-            function()
-                vim.diagnostic.setqflist({ open = true, severity = "ERROR" })
-            end,
-            { buffer = bufnr, noremap = true, silent = true, desc = "LSP: Show all errors" }
-        )
-
-        vim.keymap.set(
-            "n",
-            "<leader>lw",
-            function()
-                vim.diagnostic.setqflist({ open = true, severity = "WARN" })
-            end,
-            { buffer = bufnr, noremap = true, silent = true, desc = "LSP: Show all warnings" }
-        )
-
-        if client.name ~= 'vtsls' then
+        if client.name == format_client then
             vim.keymap.set(
                 "n",
                 "<leader>lf",
